944bfcc73e9e0594e661e36410272a46225b6116
[Ashitosh Kshirsagar] <[ashitosh.kshirsagar.work@gmail.com]>
2024-11-10 13:33:40 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import "./App.css"
+
+const Card = ({ text, info }) => {
+  return (
+    <div className='weather-card'>
+      <h3>{text}</h3>
+      <p>{info}</p>
+    </div>
+  )
+}
+
+function App() {
+
+  const [city, setCity] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [showCard, setShowCard] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  const API_KEY = "93ef1a5af98641c6b27135421240911";
+  const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
+
+  const fetchData = async () => {
+    try {
+      let res = await fetch(URL);
+      let data = await res.json();
+      console.log("data", data)
+      return data;
+    }
+    catch {
+      alert("Failed to fetch weather data");
+    }
+  }
+
+  const changeHandler = (event) => {
+    setCity(event.target.value);
+  }
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setShowCard(false);
+    setIsLoading(true);
+    let fetchedData = await fetchData();
+    try {
+      if (fetchedData.current.temp_c) {
+        setWeatherData(fetchedData);
+        console.log(fetchedData.current);
+        setIsLoading(false);
+        setShowCard(true);
+      }
+      console.log("tried")
+    } catch {
+      alert("Failed to fetch weather data");
+    }
+  }
+
+  return (
+    <div className='App'>
+      <h1>XWEATHER</h1>
+      <form onSubmit={(event) => submitHandler(event)} className='search-bar'>
+        <input required value={city} onChange={(event) => changeHandler(event)} placeholder='Enter city name' name='city' />
+        <button>Search</button>
+      </form>
+      {isLoading ? <p>Loading data…</p> : null}
+      {showCard ?
+        <div className='weather-cards'>
+          <Card text={"Temperature"} info={`${weatherData.current.temp_c} °C`} />
+          <Card text={"Humidity"} info={`${weatherData.current.humidity}%`} />
+          <Card text={"Condition"} info={`${weatherData.current.condition.text}`} />
+          <Card text={"Wind Speed"} info={`${weatherData.current.wind_kph} kph`} />
+        </div> :
+        null
+      }
+    </div>
+  )
+}
+
+export default App
